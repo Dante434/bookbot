d0bdeaf2bbf8dff9b00127bd155b91d6cefc965a
Dante434 <Zachary.dudley@protonmail.com>
2025-01-26 14:11:50 -0800

    updated main.py with word counter

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ def main():
 
 main()
 
+file_path = ("books/frankenstein/txt")
 
+def count_words_in_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            text = file.read()
+            words = text.split()  # Splits the text into words by whitespace
+            word_count = len(words)
+            print(f'Total number of words: {word_count}')
+    except FileNotFoundError:
+        print(f"The file at {file_path} was not found.")
