79dd83e2fe7d6dd28a9b1bd8dad3739865a9b345
Dante434 <Zachary.dudley@protonmail.com>
2025-01-26 14:35:16 -0800

    fixed word counter

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
-
-main()
-
-file_path = ("books/frankenstein/txt")
+file_path = 'books/frankenstein.txt'
 
 def count_words_in_file(file_path):
     try:
         with open(file_path, 'r') as file:
             text = file.read()
-            words = text.split()  # Splits the text into words by whitespace
+            words = text.split() 
             word_count = len(words)
             print(f'Total number of words: {word_count}')
     except FileNotFoundError:
         print(f"The file at {file_path} was not found.")
+
+count_words_in_file(file_path)
+
